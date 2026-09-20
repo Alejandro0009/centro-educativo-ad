811e6af81f17b22aca0f94a0ad90caf408b52f1a
Alejandro Valencia Mendoza <alejandrovalencia0009@gmail.com>
2026-09-19 23:52:55 -0600

    feat: Auth correccion de user y pass en local

--- src/app/features/staff/auth/auth-guard.ts
+++ src/app/features/staff/auth/auth-guard.ts
@@ -1,9 +1,22 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  inject,
+  PLATFORM_ID
+} from '@angular/core';
+import {
+  CanActivateFn,
+  Router
+} from '@angular/router';
 
 export const authGuard: CanActivateFn = () => {
 
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  // Durante el prerender de Angular no existe localStorage
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
 
   const authenticated =
     localStorage.getItem('ad_staff_auth') === 'true';
@@ -15,5 +28,4 @@ export const authGuard: CanActivateFn = () => {
   return router.createUrlTree([
     '/empleados/login'
   ]);
-
 };
