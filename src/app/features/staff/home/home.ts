071f242a7cdf46f7d62c9fc99d7eec19738688b6
Alejandro Valencia Mendoza <alejandrovalencia0009@gmail.com>
2026-09-19 23:49:56 -0600

    feat: home.tp correccion de user y pass en local

--- src/app/features/staff/home/home.ts
+++ src/app/features/staff/home/home.ts
@@ -1,47 +1,46 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  Inject,
+  PLATFORM_ID
+} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-staff-home',
   standalone: true,
-
-  imports: [
-    RouterLink
-  ],
-
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
 
-  username =
-    localStorage.getItem('ad_staff_user')
-    ?? 'AD-STAFF';
-
+  username = 'AD-STAFF';
 
   constructor(
-    private router: Router
-  ) {}
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
 
+    // localStorage solamente existe en el navegador
+    if (isPlatformBrowser(this.platformId)) {
+      this.username =
+        localStorage.getItem('ad_staff_user')
+        ?? 'AD-STAFF';
+    }
+  }
 
   logout(): void {
 
-    localStorage.removeItem(
-      'ad_staff_auth'
-    );
-
-    localStorage.removeItem(
-      'ad_staff_user'
-    );
-
+    // Evita errores durante SSR / prerender
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('ad_staff_auth');
+      localStorage.removeItem('ad_staff_user');
+    }
 
     this.router.navigate(
       ['/empleados/login'],
-      {
-        replaceUrl: true
-      }
+      { replaceUrl: true }
     );
-
   }
-
 }
